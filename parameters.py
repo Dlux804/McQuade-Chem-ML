8b7fb93d99836ca50c0d80eaa2c850aba756a69e
Adam Luxon <luxon.adam@gmail.com>
2019-11-21 15:33:16 -0500

    made parameter grid dictionary for specific method calls
    Signed-off-by: Adam Luxon <luxon.adam@gmail.com>

--- parameters.py
+++ parameters.py
@@ -1,6 +1,7 @@
 # TODO: ADD DOC STRINGS!!!
 
-def Adaparamgrid():
+
+def ada_paramgrid():
     base_estimator = [tree.DecisionTreeRegressor(max_features='sqrt', splitter='best', max_depth=3), GradientBoostingRegressor(), SVR(kernel = 'linear'),RandomForestRegressor(n_estimators=500)]
     n_estimators = [int(x) for x in np.linspace(start = 50, stop = 1000, num = 30)]
     learning_rate = [0.001,0.005,0.01,0.05,0.1,0.5,1]
@@ -8,7 +9,7 @@ def Adaparamgrid():
                   'n_estimators': n_estimators,
                   'learning_rate': learning_rate}
     return param_grid
-def RFparamgrid():
+def rf_paramgrid():
     n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 20)] #Number of trees
     max_features = ['auto', 'sqrt']     # Number of features to consider at every split
     max_depth = [int(x) for x in np.linspace(1, 30, num = 11)] # Maximum number of levels in tree
@@ -23,7 +24,7 @@ def RFparamgrid():
            'min_samples_leaf': min_samples_leaf,
            'bootstrap': bootstrap}
     return param_grid
-def SVRparamgrid():
+def svr_paramgrid():
 #Kernel functions
     kernel = ['rbf', 'poly', 'linear']
 #Penalty parameter C of the error term.
@@ -36,7 +37,7 @@ def SVRparamgrid():
     degrees = [1,2,3,4,5]
     param_grid = {'kernel': kernel,'C': Cs, 'gamma' : gammas, 'epsilon': epsilon, 'degree' : degrees}
     return param_grid
-def GDBparamgrid():
+def gdb_paramgrid():
 #Number of trees
     n_estimators = [int(x) for x in np.linspace(start = 500, stop = 2000, num = 20)]
 # Number of features to consider at every split
@@ -56,7 +57,8 @@ def GDBparamgrid():
            'min_samples_leaf': min_samples_leaf,
            'learning_rate': learning_rate}
     return param_grid
-def MLPparamgrid():
+
+def mlp_paramgrid():
     #Number of hidden layers
     hidden_layer_sizes = [(100,), (100,50,100), (50,100,50), (np.random.randint(low = 50, high = 100, size = 10))]
     #Activation function for the hidden layer.
@@ -76,7 +78,7 @@ def MLPparamgrid():
             }
     return param_grid
 
-def KNNparamgrid():
+def knn_paramgrid():
     #Number of neighbors to use
     n_neighbors = [int(x) for x in np.linspace(start = 5, stop = 30, num = 20)]
     #weight function used in prediction
@@ -94,3 +96,20 @@ def KNNparamgrid():
                   'p': p
             }
     return param_grid
+
+# Dictionary containing all the grid functions
+# Can call specific function based off of dict key.
+'''
+Example:
+other_function(method):
+    grid = grids(method)
+    return grid
+'''
+grids = {
+    "ada" : ada_paramgrid,
+    'rf' : rf_paramgrid,
+    'svr': svr_paramgrid,
+    'gdb': gdb_paramgrid,
+    'mlp': mlp_paramgrid,
+    'knn': knn_paramgrid
+}

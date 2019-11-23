d8c26361b388a5b0c1bc6bd0782e8eac25abc9c3
Adam Luxon <luxon.adam@gmail.com>
2019-11-23 11:37:47 -0500

    Added Hypertune, update regregssor with optimized params

--- regressors.py
+++ regressors.py
@@ -9,6 +9,8 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.ensemble.gradient_boosting import GradientBoostingRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.svm import SVR
+from sklearn.model_selection import RandomizedSearchCV
+from time import time
 
 
 def runRegress(model, model_name,train_features, test_features, train_target, test_target, expt):
@@ -23,23 +25,25 @@ def runRegress(model, model_name,train_features, test_features, train_target, te
 
 
 # Hyper parameter tunes using RandomizedSearchCV
-def hyperTune(model, model_name, train_features, train_target, grid, folds, iters, expt): # WHAT is expt? WHY use it?
+def hyperTune(model, train_features, train_target, grid, folds, iters, jobs=-1): # WHAT is expt? WHY use it?
     # TODO: Add doc string
     print("Starting Hyperparameter tuning\n")
     start_tune = time()
     search_random = RandomizedSearchCV(estimator=model, param_distributions=grid, n_iter=iters, cv=folds, verbose=2,
-                                       random_state=42, n_jobs=4)
-    log.at[exp, 'Parameter Grid'] = grid
+                                       random_state=42, n_jobs=jobs)
+    # TODO: Set up logging of parameters
+    # log.at[exp, 'Parameter Grid'] = grid
+
     # Fit the random search model
     search_random.fit(train_features, train_target)
     tuned = search_random.best_params_
-    param_dict = {str(model_name) + '-' + expt: tuned}
 
+    # Calculate time to tune parameters
     stop_tune = time()
-    time_par = {str(model_name) + '-' + expt: (stop_tune - start_tune)}
+    tune_time = stop_tune - start_tune
     print('Best Parameter Found After ', (stop_tune - start_tune), "sec\n")
     print(tuned)
-    return tuned, param_dict, time_par
+    return tuned, tune_time
 
 
 def regressor(model, tune=False):
@@ -60,7 +64,7 @@ def regressor(model, tune=False):
         'mlp': MLPRegressor,
         'knn': KNeighborsRegressor
     }
-    return regressors[model]()
+    return regressors[model]
 
 
 

31bade485f943bc4832def0fc58fc9d96f1f6b22
qle <quangle270799@gmail.com>
2019-11-24 12:40:47 -0500

    Test ingest.py

--- test_ingest.py
+++ test_ingest.py
@@ -5,6 +5,9 @@ import pandas as pd
 
 class TestIngest(unittest.TestCase):
     def test_load_smiles(self):
-        csv, smiles_col = load_smiles(self, "water-energy.csv")
-        assert type(csv) == pd.DataFrame
-        assert type(smiles_col) == pd.Series
+    """
+    This script is created to test if ingest.py is returning a csv and a SMILES column
+    """
+        csv, smiles_col = load_smiles(self, "water-energy.csv")  #Load the funtion
+        assert type(csv) == pd.DataFrame  #See if csv is a dataframe
+        assert type(smiles_col) == pd.Series  # See if smiles_col is a series

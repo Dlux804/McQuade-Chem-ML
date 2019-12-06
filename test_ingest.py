93caa5840f100c2702f97c543d18ee7e4d988505
qle <quangle270799@gmail.com>
2019-12-06 15:15:06 -0500

    Test ingest.py using mock

--- test_ingest.py
+++ test_ingest.py
@@ -3,10 +3,23 @@ from ingest import load_smiles
 import pandas as pd
 from models import MlModel
 import mock
+import pandas as pd
+
+# Mock the module Chem since it's the thing we want to test
+@mock.patch('ingest.Chem')
+def test_ingest_Chem(mock_chem):
+    """
+    Chem.MolFromSmiles is the core function used to make the function load_smiles in ingest.py. Therefore, we would like
+    to test if there's any error in using this function. This will help us narrow down the actual error if we ever run
+    into one.
+    """
+    df = pd.read_csv('water-energy.csv')
+    model_test = MlModel('rf', 'water-energy.csv', 'expt')  # Calling MlModel to get our class instances
+    mock_chem.MolFromSmiles.assert_called()
+    mock.patch.stopall()
 
 # Test load_smiles when drop = False
-@mock.patch('models.MlModel')  # Using mock to "replace" more time demanding calls like MlModel
-def test_load_smiles_dropfalse(mock_mlmodel):
+def test_load_smiles_dropfalse():
     """
     Test if ingest.py is returning a dataframe and a SMILES column when drop=False
     Using mocker (a pytest plug-in), we're able to to test for the necessary variable without having to make time intensive calls
@@ -17,10 +30,8 @@ def test_load_smiles_dropfalse(mock_mlmodel):
     # csv, smiles_col = load_smiles(model_test.data, "water-energy.csv", drop=False)
     assert type(model_test.data) == pd.DataFrame  # Testing to see if data is a dataframe
     assert type(model_test.smiles) == pd.Series  # Testing to see if we get a SMILES column
-    mock.patch.stopall()  # Stop mock object
 
-@mock.patch('models.MlModel')  # Using mock to "replace" more time demanding calls like MlModel
-def test_load_smiles_droptrue(mock_mlmodel):
+def test_load_smiles_droptrue():
     """
     Test if ingest.py is returning a dataframe and a SMILES column when drop=True
     Using mocker (a pytest plug-in), we're able to to test for the necessary variable without having to make time intensive calls
@@ -31,5 +42,4 @@ def test_load_smiles_droptrue(mock_mlmodel):
     # csv, smiles_col = load_smiles(self, "water-energy.csv", drop=True)
     assert type(model_test.data) == pd.DataFrame  # Testing to see if data is a dataframe
     assert type(model_test.smiles) == pd.Series  # Testing to see if we get a SMILES column
-    mock.patch.stopall()  # Stop mock object
 

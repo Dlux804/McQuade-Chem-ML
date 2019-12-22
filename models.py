66ad2aaabeca78afef745679d27bb163e9486312
Adam Luxon <luxon.adam@gmail.com>
2019-12-22 10:08:21 -0500

    added Bayes search to regressors.py
    Signed-off-by: Adam Luxon <luxon.adam@gmail.com>

--- models.py
+++ models.py
@@ -135,26 +135,26 @@ class MlModel:
 
 
 
-# # Initiate Model
-# model1 = MlModel('gdb', 'Lipophilicity-ID.csv', 'exp')
-#
-# # featurize data with rdkit2d
-# model1.featurization([0])
-# print(model1.feat_meth)
-#
-#
-# # Run the model with hyperparameter optimization
-# model1.run(tune=False)
-# # print('Tune Time:', model1.tuneTime)
-#
-#
-#
-# # Save results
-# model1.store()
-#
-#
-# # Must show() graph AFTER it has been saved.
-# # if show() is called before save, the save will be blank
-# # display PvA graph
-# model1.graphM.show()
+# Initiate Model
+model1 = MlModel('gdb', 'Lipophilicity-ID.csv', 'exp')
+
+# featurize data with rdkit2d
+model1.featurization([0])
+print(model1.feat_meth)
+
+
+# Run the model with hyperparameter optimization
+model1.run(tune=True)
+# print('Tune Time:', model1.tuneTime)
+
+
+
+# Save results
+model1.store()
+
+
+# Must show() graph AFTER it has been saved.
+# if show() is called before save, the save will be blank
+# display PvA graph
+model1.graphM.show()
 

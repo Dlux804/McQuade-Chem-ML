f3e2bd6157cd6f8541d561e0b6518fe4e4cd61ae
Adam Luxon <luxon.adam@gmail.com>
2019-12-22 10:54:59 -0500

    removed category parameters from grid.py
    Signed-off-by: Adam Luxon <luxon.adam@gmail.com>

--- hte-models-overhaul.py
+++ hte-models-overhaul.py
@@ -8,7 +8,13 @@ import models
 def main():
 
     # list of available learning algorithms
-    learner = ['ada', 'rf', 'svr', 'gdb', 'mlp', 'knn']
+    learner = [
+        # 'ada',
+        'rf',
+        'svr',
+        'gdb',
+        'mlp',
+        'knn']
 
     # list of available featurization methods
     featurize = [[0], [0, 2], [0, 3], [0, 4], [0,5], [0, 6], [2], [3], [4], [5], [6]]

b1cbb7ffab6f61b804d3e7dc0d723f3a2336849e
qle <quangle270799@gmail.com>
2020-01-19 10:42:34 -0500

    Add system path to test_ingest.py

--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -1,9 +1,14 @@
-from ingest import load_smiles
-# import unittest
-import pandas as pd
-from models import MlModel
+from core import ingest, models
 import mock
 import pandas as pd
+import os, sys
+from main import ROOT_DIR
+
+# before importing local modules, must add root dir to system path
+# capture location of current file (/root/tests/)
+myPath = os.path.dirname(os.path.abspath(__file__))
+# add to system path the root dir with relative notation: /../ (go up one dir)
+sys.path.insert(0, myPath + '/../')
 
 # Mock the module Chem since it's the thing we want to test
 @mock.patch('ingest.Chem')
@@ -13,10 +18,15 @@ def test_ingest_Chem(mock_chem):
     to test if there's any error in using this function. This will help us narrow down the actual error if we ever run
     into one.
     """
-    df = pd.read_csv('water-energy.csv')
-    model_test = MlModel('rf', 'water-energy.csv', 'expt')  # Calling MlModel to get our class instances
-    mock_chem.MolFromSmiles.assert_called()
-    mock.patch.stopall()
+    # change working directory to
+    os.chdir(ROOT_DIR)
+    # move to dataFiles
+    with misc.cd('dataFiles'):
+        print('Now in:', os.getcwd())
+        df = pd.read_csv('water-energy.csv')
+        model_test = models.MlModel('rf', 'water-energy.csv', 'expt')  # Calling MlModel to get our class instances
+        mock_chem.MolFromSmiles.assert_called()
+        mock.patch.stopall()
 
 # Test load_smiles when drop = False
 def test_load_smiles_dropfalse():
@@ -24,18 +34,28 @@ def test_load_smiles_dropfalse():
     Test if ingest.py is returning a dataframe and a SMILES column when drop=False
     I'm still testing on how to use mock more efficiently
     """
-    model_test = MlModel('rf', 'water-energy.csv', 'expt')  # Calling MlModel to get our class instances
-    # csv, smiles_col = load_smiles(model_test.data, "water-energy.csv", drop=False)
-    assert type(model_test.data) == pd.DataFrame  # Testing to see if data is a dataframe
-    assert type(model_test.smiles) == pd.Series  # Testing to see if we get a SMILES column
+    # change working directory to
+    os.chdir(ROOT_DIR)
+    # move to dataFiles
+    with misc.cd('dataFiles'):
+        print('Now in:', os.getcwd())
+        model_test = models.MlModel('rf', 'water-energy.csv', 'expt')  # Calling MlModel to get our class instances
+        # csv, smiles_col = load_smiles(model_test.data, "water-energy.csv", drop=False)
+        assert type(model_test.data) == pd.DataFrame  # Testing to see if data is a dataframe
+        assert type(model_test.smiles) == pd.Series  # Testing to see if we get a SMILES column
 
 def test_load_smiles_droptrue():
     """
     Test if ingest.py is returning a dataframe and a SMILES column when drop=True
     I'm still testing on how to use mock more efficiently
     """
-    model_test = MlModel('gdb', 'water-energy.csv', 'expt', drop=False)  # Calling MlModel to get our class instances, drop=False in this case
-    # csv, smiles_col = load_smiles(self, "water-energy.csv", drop=True)
-    assert type(model_test.data) == pd.DataFrame  # Testing to see if data is a dataframe
-    assert type(model_test.smiles) == pd.Series  # Testing to see if we get a SMILES column
+    # change working directory to
+    os.chdir(ROOT_DIR)
+    # move to dataFiles
+    with misc.cd('dataFiles'):
+        print('Now in:', os.getcwd())
+        model_test = models.MlModel('gdb', 'water-energy.csv', 'expt', drop=False)  # Calling MlModel to get our class instances, drop=False in this case
+        # csv, smiles_col = load_smiles(self, "water-energy.csv", drop=True)
+        assert type(model_test.data) == pd.DataFrame  # Testing to see if data is a dataframe
+        assert type(model_test.smiles) == pd.Series  # Testing to see if we get a SMILES column
 

1f3ddb0e9ffa251b547c613d40e35ec65073f830
qle <quangle270799@gmail.com>
2020-02-01 13:51:48 -0500

    Testing examples

--- padel/padel_test_v1.py
+++ padel/padel_test_v1.py
@@ -1,1 +1,14 @@
+from padelpy import from_smiles
+import pandas as pd
+# calculate molecular descriptors for propane
+descriptors = from_smiles('CCC')
+df = pd.DataFrame(descriptors, columns=descriptors.keys(), index=[0])
+print(df)
+# in addition to descriptors, calculate PubChem fingerprints
+# desc_fp = from_smiles('CCC', fingerprints=True)
 
+# only calculate fingerprints
+# fingerprints = from_smiles('CCC', fingerprints=True, descriptors=False, output_csv='fingerprints.csv')
+
+# save descriptors to a CSV file
+# _ = from_smiles('CCC', output_csv='descriptors.csv')

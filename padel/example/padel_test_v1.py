e86169cb6248b7738d820747336f2bf6e9b8dfb3
qle <quangle270799@gmail.com>
2020-02-04 20:26:25 -0500

    test examples

--- padel/example/padel_test_v1.py
+++ padel/example/padel_test_v1.py
@@ -1,16 +1,14 @@
 from padelpy import from_smiles
 import pandas as pd
 import os
+from descriptastorus.descriptors.DescriptorGenerator import MakeGenerator
 
 """
 Objective: We want to test if the example codes given in padel's github actually works. We also want to understand the
 ins and outs of this module. 
-Everytime the code is run, a .smi file is formed
 """
 
-current_dir = "C:/Users/quang/McQuade-Chem-ML/padel/example"
-# calculate molecular descriptors for propane
-descriptors = from_smiles('C1=CC=CC=C1', output_csv='descriptors.csv')
+descriptors = from_smiles('C1=CC=CC=C1', output_csv='descriptors.cs')
 df = pd.DataFrame(descriptors, columns=descriptors.keys(), index=[0])
 print(df)
 # for file in os.listdir(current_dir):
@@ -20,7 +18,7 @@ print(df)
 # desc_fp = from_smiles('CCC', fingerprints=True)
 
 # only calculate fingerprints
-fingerprints = from_smiles('CCC', fingerprints=True, descriptors=False, output_csv='fingerprints.csv')
+# fingerprints = from_smiles('CCC', fingerprints=True, descriptors=False, output_csv='fingerprints.csv')
 
 # save descriptors to a CSV file
 # _ = from_smiles('CCC', output_csv='descriptors.csv')

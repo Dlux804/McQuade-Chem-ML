c033655d7ca28a63719c04e1e91c912650c29867
Andres Hyer <hyerap@vcu.edu>
2020-02-09 10:51:41 -0500

    separate functionality from calling, all classes keep in backends or chemneo4j files

--- Neo4j/BulkChem/main.py
+++ Neo4j/BulkChem/main.py
@@ -1,4 +1,5 @@
-from Neo4j.BulkChem.ChemNeo4j_v2 import init_neo_bulkchem, create_relationships
+from Neo4j.BulkChem.ChemNeo4j_v3 import create_relationships
+from Neo4j.BulkChem.backends import init_neo_bulkchem
 
 init_neo_bulkchem()
-create_relationships('B', retrieve_data_from_neo4j=True)
+create_relationships('Default', retrieve_data_from_neo4j=True)

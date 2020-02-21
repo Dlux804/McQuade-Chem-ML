3d1271792e25d96bec0e8e31325c6d9a43acb85d
qle <quangle270799@gmail.com>
2020-02-21 10:43:21 -0500

    Finish script

--- neo4j/make_labels.py
+++ neo4j/make_labels.py
@@ -33,9 +33,12 @@ class Labels:
         """
         df = pd.read_csv(csv, index_col=0)
         df = df[df.columns.dropna()]
+        algo_sries = df["algorithm"]
         col_lst = df.columns.tolist()
         col = col_lst[0:10]
         new_df = df.drop(columns=col)
+        new_df["algorithm"] = algo_sries.tolist()
+        # print(new_df)
         header = new_df.columns.tolist()  # List of all columns in dataframe
         label_lst = []  # List of lists of all the labels
         for i in header:
@@ -88,16 +91,17 @@ class Labels:
             header_lst.append(new_item)
         final_df = pd.DataFrame(array_rotate, columns=header_lst)
         # final_df = df.assign(algorithm=algo_series)
-        print(final_df)
+        # print(final_df)
         return final_df
 
 
-def final_modeldf(header, label_lst):
+def label_model_todf(csv):
     """
 
     :param csv:
     :return:
     """
+    header, label_lst = Labels.model_label_tolist(csv)
     df = Labels.label_df(header, label_lst)
     enum_col = df["algorithmRun#"]
     header_lst = []
@@ -118,9 +122,35 @@ def final_modeldf(header, label_lst):
     return results_df
 
 
-# header, label_lst = Labels.param_label_tolist('ml_results3.csv', "gdb")
-# final_modeldf(header, label_lst)
-# final_df('ml_results3.csv')
+def label_param_todf(csv, algo):
+    """
+
+    :param csv:
+    :return:
+    """
+    header, label_lst = Labels.param_label_tolist(csv, algo)
+    df = Labels.label_df(header, label_lst)
+    enum_col = df["algorithmRun#"]
+    header_lst = []
+    for i in add_col:
+        l = 1
+        lst = []
+        label = i + " #"
+        for row in enum_col:
+            new_label = label + str(l)
+            l = l + 1
+            lst.append(new_label)
+        header_lst.append(lst)
+    rotated_lst = list(rotated(header_lst))
+    array_rotate = np.array(rotated_lst)
+    add_df = pd.DataFrame(array_rotate, columns=add_col)
+    results_df = pd.concat([df, add_df], axis=1)
+    # print(results_df)
+    return results_df
+
+
+# header, label_lst = Labels.model_label_tolist('ml_results3.csv')
+label_model_todf('ml_results3.csv')
 
 
 

f5c8c0fe73f20a284c657c34b3d4702993a2dc70
qle <quangle270799@gmail.com>
2020-02-20 22:30:43 -0500

    class for making labels

--- neo4j/make_labels.py
+++ neo4j/make_labels.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import extract_params as ep
 """
 Objective: Make dataframes that will contain names 
             and labels for knowledge graphs using a dataframe that only has values 
@@ -18,60 +19,87 @@ def rotated(array_2d):
     return [list(elem) for elem in list_of_tuples]
 
 
-def label_tolist(csv):
+class Labels:
+    def __init__(self, file):
+        self.file = file
+
+    @staticmethod
+    def model_label_tolist(csv):
+        """
+        Objective: Create lists of all the labels I want to make from our ml results inside the csv. The goal is to make
+        labels for
+        :param csv:
+        :return:
+        """
+        df = pd.read_csv(csv, index_col=0)
+        df = df[df.columns.dropna()]
+        col_lst = df.columns.tolist()
+        col = col_lst[0:10]
+        new_df = df.drop(columns=col)
+        header = new_df.columns.tolist()  # List of all columns in dataframe
+        label_lst = []  # List of lists of all the labels
+        for i in header:
+            series = new_df[i]
+            lst = []
+            label = i + '#'
+            j = 1
+            for row in series:
+                new_label = label + str(j)
+                j = j + 1
+                lst.append(new_label)
+            label_lst.append(lst)
+        return header, label_lst
+
+    @staticmethod
+    def param_label_tolist(csv, algo):
+        """
+
+        :param csv:
+        :return:
+        """
+        df = ep.param_finaldf(csv, algo)
+        header = df.columns.tolist()  # List of all columns in dataframe
+        label_lst = []  # List of lists of all the labels
+        for i in header:
+            series = df[i]
+            lst = []
+            label = i + '#'
+            j = 1
+            for row in series:
+                new_label = label + str(j)
+                j = j + 1
+                lst.append(new_label)
+            label_lst.append(lst)
+        return header, label_lst
+
+    @staticmethod
+    def label_df(header, label_lst):
+        """
+
+        :param header:
+        :param label_lst:
+        :return:
+        """
+        rotated_lst = list(rotated(label_lst))
+        array_rotate = np.array(rotated_lst)
+        header_lst = []
+        for i in header:
+            new_item = i + 'Run#'
+            header_lst.append(new_item)
+        final_df = pd.DataFrame(array_rotate, columns=header_lst)
+        # final_df = df.assign(algorithm=algo_series)
+        print(final_df)
+        return final_df
+
+
+def final_modeldf(header, label_lst):
     """
 
     :param csv:
     :return:
     """
-    df = pd.read_csv(csv, index_col=0)
-    algo_series = df["algorithm"]
-    df = df[df.columns.dropna()]
-    col_lst = df.columns.tolist()
-    col = col_lst[0:10]
-    new_df = df.drop(columns=col)
-    header = new_df.columns.tolist()
-    label_lst = []
-    for i in header:
-        series = new_df[i]
-        lst = []
-        label = i+'#'
-        j = 1
-        for row in series:
-            new_label = label + str(j)
-            j = j+1
-            lst.append(new_label)
-        label_lst.append(lst)
-    return header, label_lst, algo_series
-
-
-def label_df(csv):
-    """
-
-    :param csv:
-    :return:
-    """
-    header, label_lst, algo_series = label_tolist(csv)
-    rotated_lst = list(rotated(label_lst))
-    array_rotate = np.array(rotated_lst)
-    header_lst = []
-    for i in header:
-        new_item = i + ' Run#'
-        header_lst.append(new_item)
-    df = pd.DataFrame(array_rotate, columns=header_lst)
-    final_df = df.assign(algorithm=algo_series)
-    print(final_df)
-    return final_df
-
-
-def final_df(csv):
-    """
-
-    :param csv:
-    :return:
-    """
-    df = label_df(csv)
-    enum_col = df["algorithm"]
+    df = Labels.label_df(header, label_lst)
+    enum_col = df["algorithmRun#"]
     header_lst = []
     for i in add_col:
         l = 1
@@ -79,7 +107,7 @@ def final_df(csv):
         label = i + " #"
         for row in enum_col:
             new_label = label + str(l)
-            l = l+1
+            l = l + 1
             lst.append(new_label)
         header_lst.append(lst)
     rotated_lst = list(rotated(header_lst))
@@ -90,6 +118,8 @@ def final_df(csv):
     return results_df
 
 
+# header, label_lst = Labels.param_label_tolist('ml_results3.csv', "gdb")
+# final_modeldf(header, label_lst)
 # final_df('ml_results3.csv')
 
 

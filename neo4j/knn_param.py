2b04c1934034d7e7dc65bb841486cadf8f3a1d0f
qle <quangle270799@gmail.com>
2020-02-23 10:33:04 -0500

    Fix merging

--- neo4j/knn_param.py
+++ neo4j/knn_param.py
@@ -5,7 +5,7 @@ import extract_params as ep  # Extract parameters from ml results
 import make_labels as ml  # Make label dataframes
 
 
-def graph_adaparam(model_csv, algor="knn"):
+def graph_knnparam(model_csv, algor="knn"):
     """
 
     :param model_csv:
@@ -142,21 +142,21 @@ def graph_adaparam(model_csv, algor="knn"):
         tx.merge(be)
         loop_param = param_dct[i]
         leaf_size_label = Node("leaf_size_label", leaf_size_labels=loop_param['leaf_sizeRun#'])
-        tx.merge(leaf_size_label)
+        tx.create(leaf_size_label)
         leaf_size = Node("leaf_size", leaf_sizes=loop_param['leaf_size'])
-        tx.merge(leaf_size)
+        tx.create(leaf_size)
         n_neighbors_label = Node("n_neighbors_label", n_neighbors_labels=loop_param['n_neighborsRun#'])
-        tx.merge(n_neighbors_label)
+        tx.create(n_neighbors_label)
         n_neighbors = Node("n_neighbors", n_neighbor=loop_param['n_neighbors'])
-        tx.merge(n_neighbors)
+        tx.create(n_neighbors)
         p_label = Node("p_label", p_labels=loop_param['pRun#'])
-        tx.merge(p_label)
+        tx.create(p_label)
         p = Node("p", ps=loop_param['p'])
-        tx.merge(p)
+        tx.create(p)
         weights_label = Node("weights_label", weights_labels=loop_param['weightsRun#'])
-        tx.merge(weights_label)
+        tx.create(weights_label)
         weights = Node("weights", weight=loop_param['weights'])
-        tx.merge(weights)
+        tx.create(weights)
         bf = Relationship(regressor, "has", leaf_size_label)
         tx.merge(bf)
         bg = Relationship(leaf_size_label, "is", leaf_size)

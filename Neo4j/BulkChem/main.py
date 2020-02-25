29be164af8b05d6227c270f5bc277e0440e9d64a
Andres Hyer <hyerap@vcu.edu>
2020-02-25 17:45:09 -0500

    wrapper file for easy to use of ChemNeo4j classes

--- Neo4j/BulkChem/main.py
+++ Neo4j/BulkChem/main.py
@@ -2,4 +2,4 @@ from Neo4j.BulkChem.ChemNeo4j_v4 import create_relationships
 from Neo4j.BulkChem.backends import init_neo_bulkchem
 
 init_neo_bulkchem(fragments_as_nodes=False)
-create_relationships('pka')
+create_relationships('strict_fragments')

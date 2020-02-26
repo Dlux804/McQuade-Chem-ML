58b03db05be96ac8df147e398ec16f0b1476cbd6
Adam Luxon <luxon.adam@gmail.com>
2020-02-26 16:49:05 -0500

    Added graphing function with decent flexibility. Plots RMSE vs algorithm in group bar plot. Groups of bars show difference in featurization method. Still needs to add error bars and tune fine points on appearance.

--- core/compare.py
+++ core/compare.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from time import time
 import pandas as pd
@@ -13,32 +14,13 @@ from rdkit import Chem
 from rdkit.Chem import Draw
 from rdkit.Chem import PandasTools
 import itertools
+import pprint
 
 
 """
 Goal is to compare the output of many models in a systematic, automatic manner.
 """
 
-def merger():
-    """ Function to combine all results csv  into a single file. """
-
-    #import csv files from folders
-    path = r'C:/Users/luxon/OneDrive/Research/McQuade/Projects/NSF/OKN/phase1/Work/ml-hte-results-20200207'  # will vary by OS, computer
-    allFiles = glob.glob(path + "/*/*.csv")
-    with open('hte-models-Master-Results.csv', 'wb') as outfile:
-        for i, fname in enumerate(allFiles):
-            with open(fname, 'rb') as infile:
-                if i != 0:
-                    infile.readline()  # Throw away header on all but first file
-                # Block copy rest of file from input to output without parsing
-                shutil.copyfileobj(infile, outfile)
-                print(fname + " has been imported.")
-
-    master = pd.read_csv('hte-models-Master-Results.csv')#, dtype={'exp': object})
-    # print(master)
-
-    short = master.drop(['pvaM', 'regressor', 'feature_list'], axis=1)
-    print(short)
 
 def datasize(dataset):
     """Cacluate dataset size by counting rows in CSV. Accept filename (string) return int.
@@ -53,10 +35,49 @@ def datasize(dataset):
             return size
 
 
+def merger():
+    """ Function to combine all results csv  into a single file. """
+
+    #import csv files from folders
+    path = r'C:/Users/luxon/OneDrive/Research/McQuade/Projects/NSF/OKN/phase1/Work/ml-hte-results-20200207'  # will vary by OS, computer
+    allFiles = glob.glob(path + "/*/*.csv")
+    with open('hte-models-Master-Results.csv', 'wb+') as outfile:
+        for i, fname in enumerate(allFiles):
+            with open(fname, 'rb') as infile:
+                if i != 0:
+                    infile.readline()  # Throw away header on all but first file
+                # Block copy rest of file from input to output without parsing
+                shutil.copyfileobj(infile, outfile)
+                # print(fname + " has been imported.")
+
+    mdf = pd.read_csv('hte-models-Master-Results.csv')#, dtype={'exp': object})
+    # print(master)
+
+    # grab unique datasets into a set
+    dsets = set(mdf['dataset'])
+
+    # create dictionary of datasets and their size
+    sizes = {}
+    for data in dsets:
+        sizes.update({data: datasize(data)})
+
+    # map the dictionary to create a new column of dataset size
+    mdf['data_size'] = mdf['dataset'].map(sizes)
+    # print(mdf.head(3))
+
+    # short = mdf.drop(['pvaM', 'regressor', 'feature_list'], axis=1)
+    # print(short)
+
+    return mdf
+
+mdf = merger()
+
+
 
 # string = 'ESOL.csv'
 # datasize(string)
 
+# obsolete
 def model_compare(master_csv):
     """
     Specific function for analyzing and comparing the outputs of ML models.
@@ -78,10 +99,200 @@ def model_compare(master_csv):
     print(mdf.head(3))
 
 
-model_compare('hte-models-Master-Results.csv')
+def alg_vs_acc(df):
+    """
+    Graphing function for comparing model algorithm vs accuracy.
+    Produceds one graph per each data set in passed dataframe.
+    Graph will have algorithm on x-axis, RMSE on y-axis.  Bar color represents featurization methods.
+    """
+    df = df.filter(['algorithm', 'dataset', 'feat_meth', 'rmse_avg', 'rmse_std'])
+
+    # just for trouble shooting and dev, filter
+    df = df[df['dataset'] != 'logP14k.csv']
+
+    datasets = list(set(df['dataset']))
+    print("\nThe imported dataframe contains entries for the following {} datasets: {}".format(len(datasets), datasets))
+
+    print(df)
+
+    for data in datasets:  # loop through each dataset
+        print('\nUsing:', data)
+        # filter df ML runs that used particular dataset
+        df_new = df[df['dataset'] == data]
+        print(df_new)
+
+        # grab featurization methods used
+        feats = list(set(list(df_new['feat_meth'])))
+        # len(feats) is how many bars there will be per algorithm
+        algs = list(set(df_new['algorithm']))
+        # print("Algorithms set: ", algs)
+        # len(algs) is how many groups (xticks) of bars there will be.
+        # data for graph will be an len(feats) by len(algs) array.
+        # data[0] will be a 1D array containing the rmse values for a featurization method across alg types
+
+        matrix = []  # empty list that we will append arrays to for each feat method
+        di = {}
+
+        for feat in feats: # for every featurization method used on this dataset...
+            print("Featurizations:", feats)
+            # get the RMSE value and put in a list
+            # use .query to filter df for feat, then grab rmse column
+            rmse = list(df_new.query('feat_meth == @feat')['rmse_avg'])  # use @ to ref a variable
+            alg =  list(df_new.query('feat_meth == @feat')['algorithm'])
+            print("algs:", alg)
+            # print("rmse values: ", rmse)
+            d = dict(zip(algs, rmse))
+            """
+            Using a dictionary because sets lose order and rmse values have no promise to be 
+            ordered in the same way as the algorithms.  Then I nested the dictonaries because otherwise, the 
+            key:value pairs would be overwritten for each iteration through the featurization methods 
+            (algorithm is key and RMSE is value).
+            
+            This seems like its going to be a nightmare to to put into matplotlib.  Also, this level of structure
+            pretty much brings us back to a dataframe... Is there an easier way to access the specific data I want
+            for each series or bars directly from the dataframe instead of pulling it out of the data frame and 
+            restructuring it?  
+            """
+            print('RMSE for data set {} using featurization {} is: {}'.format(data, feat, rmse))
+
+            # add RMSE data to the rmse matrix
+            matrix.append(rmse)
+
+            di.update({feat:d})
+            print("RMSE matrix:", matrix)
+            # print("RMSE Dictonary after adding data for {}: ".format(feat), di)
+            print()
 
 
 
+        # start graphing
+        # https://subscription.packtpub.com/book/big_data_and_business_intelligence/9781849513265/1/ch01lvl1sec16/plotting-multiple-bar-charts
+        fig, ax = plt.subplots()
+        labels = alg
+        gap = 1 / len(matrix)
+        space = 0.5 / len(matrix)
+        # gap = 0.3
+        width = (1 - space) / (len(matrix))  # from http://emptypipes.org/2013/11/09/matplotlib-multicategory-barchart/
+        # indeces = range(1, len(alg) + 1)
+
+        def autolabel(rects):
+            "Attach a text label above each bar in *rects*, displaying its height."
+            for rect in rects:
+                height = rect.get_height()
+                ax.annotate('{:.2f}'.format(height),
+                            xy=(rect.get_x() + rect.get_width() / 2, height),
+                            xytext=(0, 3),  # 3 points vertical offset
+                            textcoords="offset points",
+                            ha='center', va='bottom')
+
+        for i, row in enumerate(matrix):
+            print(alg, i)
+            X = np.arange(len(row))
+            pos = [x - (1 - space) / 2. + i * width for x in X]
+            print(pos)
+            # from http://emptypipes.org/2013/11/09/matplotlib-multicategory-barchart/
+
+            # rects = plt.bar(X + i * gap, row,
+            #         width=gap, label=feats[i-1], align='center')  #,
+            #         # color=color_list[i % len(color_list)])
+            rects = plt.bar(pos, row,
+                            width=width, label=feats[i - 1])  # ,
+            # color=color_list[i % len(color_list)])
+            autolabel(rects)
+            # rects1 = ax.bar(x - width / 2, men_means, width, label='Men')
+            # rects2 = ax.bar(x + width / 2, women_means, width, label='Women')
+
+        x = np.arange(len(labels))  # the label locations
+        ax.set_ylabel('RMSE')
+        ax.set_title('{}: Algorithm vs RMSE'.format(data))
+        ax.set_xticks(x)
+        # ax.set_xticks(indeces)
+        ax.set_xticklabels(labels)
+        ax.legend()
+
+
+
+        plt.show()
+        plt.close()
+
+        # each feat method will need to have an array of RMSE values
+        # order must match the order of algorithms (x ticks)
+        """
+        # plotting bars from a dict
+        plt.bar(range(len(D)), list(D.values()), align='center')
+        plt.xticks(range(len(D)), list(D.keys()))
+        """
+
+
+    """
+    # bar plot orange and blue
+    labels = set(df['algorithm'])
+    print(len(labels))
+    men_means = [20, 34, 30, 35]
+    women_means = [25, 32, 34, 20]
+
+    x = np.arange(len(labels))  # the label locations
+    width = 0.35  # the width of the bars
+
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width / 2, men_means, width, label='Men')
+    rects2 = ax.bar(x + width / 2, women_means, width, label='Women')
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Scores')
+    ax.set_title('Scores by group and gender')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend()
+
+    def autolabel(rects):
+        "Attach a text label above each bar in *rects*, displaying its height."
+        for rect in rects:
+            height = rect.get_height()
+            ax.annotate('{}'.format(height),
+                        xy=(rect.get_x() + rect.get_width() / 2, height),
+                        xytext=(0, 3),  # 3 points vertical offset
+                        textcoords="offset points",
+                        ha='center', va='bottom')
+
+    autolabel(rects1)
+    autolabel(rects2)
+
+    fig.tight_layout()
+
+    plt.show()
+    """
+
+alg_vs_acc(mdf)
+'Code for stacked 3D bar plot'
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection = "3d")
+#
+# ax.set_xlabel("Algorithm")
+# ax.set_ylabel("Featurization")
+# ax.set_zlabel("RMSE")
+# # ax.set_xlim3d(0,10)
+# # ax.set_ylim3d(0,10)
+#
+# # xpos = [2,5,8,2,5,8,2,5,8]
+# xpos = df['algorithm']
+# # ypos = [1,1,1,5,5,5,9,9,9]
+# ypos = df['feat_meth']
+# # zpos = np.zeros(9)
+# zpos = np.zeros(df.shape[0])
+#
+# dx = np.ones(9)
+# dy = np.ones(9)
+# dz = [np.random.random(9) for i in range(4)]  # the heights of the 4 bar sets
+#
+# _zpos = zpos   # the starting zpos for each bar
+# colors = ['r', 'b', 'g', 'y']
+# for i in range(4):
+#     ax.bar3d(xpos, ypos, _zpos, dx, dy, dz[i], color=colors[i])
+#     _zpos += dz[i]    # add the height of each bar to know where to start the next
+#
+# plt.gca().invert_xaxis()
+# plt.show()
 
 def moloverlap(datasets):
     """
@@ -189,3 +400,16 @@ data = {
 
 
 
+
+# data = [[5., 25., 50., 20.],
+#   [4., 23., 51., 17.],
+#   [6., 22., 52., 19.]]
+#
+# color_list = ['b', 'g', 'r']
+# gap = .8 / len(data)
+# for i, row in enumerate(data):
+#   X = np.arange(len(row))
+#   plt.bar(X + i * gap, row,
+#     width = gap,
+#     color = color_list[i % len(color_list)])
+#

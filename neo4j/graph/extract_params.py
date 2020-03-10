4756d365a87c5aae18c90edc99a0e19457b3df2e
qle <quangle270799@gmail.com>
2020-03-09 23:30:10 -0400

    Fix up functions

--- neo4j/graph/extract_params.py
+++ neo4j/graph/extract_params.py
@@ -39,19 +39,18 @@ def rotated(array_2d):
 
 
 class Params:
-
     @staticmethod
-    def param_extract(csv, algor):
+    def clean_param(csv):
         """
         Extract parameters and put them into a list
         :param csv:
-        :param algor
         :return:
         """
-        df = pd.read_csv(csv)
-        df_clean = df[df.tuneTime != 0]
-        df_algor = df_clean[df_clean.algorithm == algor]
-        dct = df_algor.to_dict('records')
+        pre_df = pd.read_csv(csv, index_col=0)
+        df_clean = pre_df.dropna()
+        df = df_clean.reset_index(drop=True)
+        # df_algor = df[df.algorithm == algor]
+        dct = df.to_dict('records')
         param_list = []
         param_clean = []
         for i in range(len(dct)):
@@ -61,12 +60,12 @@ class Params:
             reg = params[params.find("(") + 1:params.find(")")]
             new_reg = " ".join(reg.split())
             final_reg = new_reg.replace("'", "")
-            # print(final_reg)
             element = final_reg.split(",")
             param_list.append(element)
             param_clean.append(final_reg)
-        # print(param_list)
-        return df_algor, param_list, param_clean
+        drop_df = df.drop(columns='regressor')
+        final_df = drop_df.assign(regressor=param_clean)
+        return final_df, param_list
 
     @staticmethod
     def param_lst(csv, algor):
@@ -76,27 +75,26 @@ class Params:
         :param algor
         :return:
         """
-        df_algor, param_list, param_clean = Params.param_extract(csv, algor)
-        # df_results = df_clean[df_clean.algorithm == algor]
-        runs_idx = df_algor["algorithm"]
+        pre_df, param_list = Params.clean_param(csv)
+        df = pre_df.assign(param_lst=param_list)
+        df_algor = df[df.algorithm == algor]
+        param_list = df_algor["param_lst"].tolist()
+        algor_lst = df_algor["algorithm"].tolist()
+        regress_lst = df_algor['regressor'].tolist()
         param_df = pd.DataFrame.from_records(param_list, columns=param_dict(algor))
         unique_df = param_df.loc[:, ~(param_df == param_df.iloc[0]).all()]
         # print(unique_df)
-        runs_lst = runs_idx.tolist()
-        df_new_col = unique_df.assign(algorithm=runs_lst)
+        final_df = unique_df.assign(algorithm=algor_lst, regressor=regress_lst)
         # print(df_with_algor)
-        col_list = df_new_col.columns.tolist()
+        col_list = final_df.columns.tolist()
         main_list = []
         for col in col_list:
             col_lst = []
-            for i in df_new_col[col]:
-                if i != None:
-                    t = re.sub('.*=', '', i)
-                    col_lst.append(t)
-                else:
-                    continue
+            for i in final_df[col]:
+                t = re.sub('.*=', '', i)
+                col_lst.append(t)
             main_list.append(col_lst)
-        return col_list, main_list, param_clean
+        return col_list, main_list
 
     def param_df(self, csv, algor):
         """
@@ -105,18 +103,15 @@ class Params:
         :param algor:
         :return:
         """
-        col_list, main_list, param_clean = Params.param_lst(csv, algor)
+        col_list, main_list = Params.param_lst(csv, algor)
         rotate_lst = list(rotated(main_list))
         array_rotate = np.array(rotate_lst)
         df_records = pd.DataFrame.from_records(array_rotate, columns=col_list)
-        final_df = df_records.assign(regressor=param_clean)
-        # print("Parameter Dataframe\n")
-        # print(final_df)
         # final_df.to_csv("test.csv")
-        return final_df
+        return df_records
 
 # params = Params()
-# params.param_extract('ml_results3.csv', "gdb")
+# params.param_extract('ml_results3.csv')
 # params.param_lst('ml_results3.csv', "gdb")
 # params.param_df('ml_results3.csv', "gdb")
 

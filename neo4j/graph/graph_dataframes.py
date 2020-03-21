2c531ec2d361ca8cd27caada760957ad163275e1
qle <quangle270799@gmail.com>
2020-03-20 22:12:02 -0400

    Fix dataframe

--- neo4j/graph/graph_dataframes.py
+++ neo4j/graph/graph_dataframes.py
@@ -11,18 +11,23 @@ import pandas as pd
 
 class GraphDataframe:
     @staticmethod
-    def model_dataframe(csv):
+    def model_dataframe(csv, algor):
         """
         Objective: Create dataframe with all values and labels for machine learning results.
         :param csv: csv file
+        :param algor: algorithm
         :return: final_df: Final dataframe with all values and labels for machine learning results.
         """
         param = params.Params()  # Initiate class instance
         df, _ = param.clean_param(csv)  # Get clean param
-        label_df = labels.label_model_todf(csv)  # df with all label of ml models
-        final_df = pd.concat([df, label_df], axis=1)  # concat both models
-        # final_df = df_drop.assign(regressor=param_clean)
+        df_algor = df[df.algorithm == algor]  # Select results with a specific algorithm
+        df_reset = df_algor.reset_index(drop=True)
+        label_df = labels.label_model_todf(csv, algor)  # df with all label of ml models
+        drop_df = label_df.drop('algorithm', axis=1)
+        # final_df = df_reset.merge(label_df)
+        final_df = pd.concat([df_reset, drop_df], axis=1)  # concat both models
         print("Final Model Dataframe for Graphing")
+        # final_df.to_csv('final_df.csv')
         print(final_df)
         return final_df
 
@@ -34,11 +39,16 @@ class GraphDataframe:
         :param algor: algorithm
         :return: final_df: dataframe with values and labels for parameters
         """
+        param = params.Params()  # Initiate class instance
+        df, _ = param.clean_param(csv)  # Get clean param
+        df_algor = df[df.algorithm == algor]
+        clean_param = df_algor['regressor'].tolist()
         param_df = params.Params().param_df(csv, algor)
         label_df = labels.label_param_todf(csv, algor)
-        final_df = pd.concat([param_df, label_df], axis=1)
+        concat_df = pd.concat([param_df, label_df], axis=1)
+        final_df = concat_df.assign(regressor=clean_param)
         print("Final Label Dataframe for Graphing")
-        # full_labeldf.to_csv('label_test.csv')
+        # final_df.to_csv('label_test.csv')
         print(final_df)
         return final_df
 

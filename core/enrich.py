ea1cade9b9987cc0e5070ad197f0064ee13136aa
Adam Luxon <luxon.adam@gmail.com>
2020-04-02 13:50:41 -0400

    updated enrich.py to have first version of header file creation for apoc.import.csv.

--- core/enrich.py
+++ core/enrich.py
@@ -9,15 +9,16 @@ Just so we have the featurized datasets (using RDKit).
 from core import models
 from core.misc import cd
 import os
+import csv
 import pandas as pd
 
 sets = {
         # 'Lipophilicity-ID.csv': 'exp',
         # 'ESOL.csv': 'water-sol',
-        # 'water-energy.csv': 'expt',
+        'water-energy.csv': 'expt'
         # 'logP14k.csv': 'Kow',
         # 'jak2_pic50.csv': 'pIC50',
-        'flashpoint2.csv': 'flashpoint'
+        # 'flashpoint2.csv': 'flashpoint'
     }
 alg = 'rf'
 method = [0]
@@ -39,8 +40,42 @@ for data, target in sets.items():  # loop over dataset dictionary
         # featurize molecules
         model.featurization(method)
         print(model.data)
-        model.data = model.data.drop(columns=['RDKit2D_calculated'], axis=1)
-        model.data = model.data[['smiles', target, 'ExactMolWt', 'HeavyAtomCount', 'qed']]
+        # model.data = model.data.drop(columns=['RDKit2D_calculated'], axis=1)
+        # model.data = model.data[['smiles', target, 'ExactMolWt', 'HeavyAtomCount', 'qed']]
         model.data.to_csv('featurized-' + data, index=False)
+        node = model.data
+
+    types_dict = node.dtypes.to_dict()
+    print(types_dict)
+    node['smiles'] = node.smiles.astype(str)
+    types_dict = node.dtypes.to_dict()
+    cols = list(node.columns)
+    print(cols)
+
+
+    print(types_dict['RDKit2D_calculated'].name, type(types_dict['smiles'].name))
+    # print(type(types_dict['smiles'].name))
+
+type_dict = {
+    'object': ':STRING',  # strings are stored as objects in numpy
+    'float64': ':FLOAT',
+    'int64': ':INT',
+    'bool': ':BOOLEAN'
+}
+
+header = ['smiles:ID']
+cols = cols[1:]  # get rid of smiles column header.  Done explicitly
+for col in cols:
+    head = col + type_dict[types_dict[col].name]
+    header.append(head)
+print(header)
+
+# headwrt = csv.writer(open('featurized-ESOL-header.csv', 'wb'), delimiter=',')
+# headwrt.writerow(header)
+# headwrt.close()
+
+with open('featurized-ESOL-header.csv', 'w', newline='') as myfile:
+    wr = csv.writer(myfile)  #, quoting=csv.QUOTE_ALL)
+    wr.writerow(header)
 
 
--- neo4j/datasets-gdb.py
+++ neo4j/datasets-gdb.py
@@ -1,3 +1,9 @@
+"""
+To create a graph database in Neo4j that contains our datasets.
+Molecules should be nodes and datasets should be nodes.  They should be related.
+Molecules should have properties that are RDKit features.
+"""
+
 from py2neo import Graph, Node, Relationship
 
 
@@ -5,3 +11,17 @@ from py2neo import Graph, Node, Relationship
 
 
 graph = Graph("bolt://localhost:7687", user="neo4j", password="goku")
+
+
+"""
+Need to create formatted csv files for nodes and relationships for use in apoc.import.csv
+
+Header for node would look something like this:
+smiles:ID(Molecule-ID),name:STRING,MolWt:INT, logP:FLOAT
+
+Header for relationship file would look like this
+:START_ID(Molecule-ID),:END_ID(Molecule-ID),solubility:FLOAT
+
+"""
+
+

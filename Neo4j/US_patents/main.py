4b6c1532b4bf2d1e53ba5016538ce205074464bf
Andres Hyer <hyerap@vcu.edu>
2020-04-03 17:44:03 -0400

    Main file for Us_patents to Neo4j

--- Neo4j/US_patents/main.py
+++ Neo4j/US_patents/main.py
@@ -3,15 +3,25 @@ from Neo4j.US_patents.file_to_neo4j import file_to_neo4j
 import pandas as pd
 import os
 
-US_grants_directory_to_csvs('C:/Users/User/Desktop/5104873')
 
-head_directory = 'C:/Users/User/Desktop/5104873'
-for main_directories in os.listdir(head_directory):
-    main_directories = head_directory + '/' + main_directories
+US_patents_directory = 'C:/Users/User/Desktop/5104873'
+
+'''
+The above line should be the only line you should only have to edit to make this script work. For the US patents 
+directory, make sure you only have the folders 'grants' and 'applications' saved in the directory. The script is
+only designed to work for a directory containing only those unzipped folders. 
+
+Then afterward, start up a new neo4j graph with the password as 'password' and the script will do the rest :D.
+*Note this script is dirty and inefficient, efficiency is being worked out. This is only for prototyping. 
+'''
+
+US_grants_directory_to_csvs(US_patents_directory)
+for main_directories in os.listdir(US_patents_directory):
+    main_directories = US_patents_directory + '/' + main_directories
     for directories in os.listdir(main_directories):
         if directories[-4:] == '_csv':
             directory = main_directories + '/' + directories
-            print(directory)
+            print(directory) # Let user how far the script has made it
             for file in os.listdir(directory):
                 file = directory + '/' + file
                 try:

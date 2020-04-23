c66bd8174d9626d4638675d1b1936e32cee360cf
Andres Hyer <hyerap@vcu.edu>
2020-04-23 18:48:56 -0400

    Wrapper file to interact with bulk_insert_main.py

---
+++ Neo4j/US_patents/bulk_insert_main.py
@@ -0,0 +1,7 @@
+from Neo4j.US_patents.US_patents_xml_to_csv import US_grants_directory_to_csvs, clean_up_checker_files
+from Neo4j.US_patents import bulk_insert
+
+US_patents_directory = 'C:/Users/User/Desktop/5104873'  # Change this to location of directory
+# US_grants_directory_to_csvs(US_patents_directory)
+# clean_up_checker_files()
+bulk_insert.US_patents_to_neo(US_patents_directory)

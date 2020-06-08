757bf96ac988175e80f5030d174563e9ec95eaf1
Andres Hyer <hyerap@vcu.edu>
2020-06-08 12:18:20 -0400

    Up to US_Patents in Neo4j

--- Neo4j/US_patents/Dev.py
+++
@@ -1,6 +0,0 @@
-from Draw import drawer
-from Neo4j.US_patents.backends import map_rxn_functional_groups
-
-rxn_smarts = ("OC1O[C@H](O)[C@H](O)[C@H]1CO.C1=CC=CC=C1CCl>"
-              "OS(=O)[O-].[Na+].BrBr.C(=O)(O)[O-].[Na+].[H]O[H].CCO>"
-              "O=C1[C@H](OCC2=CC=CC=C2)[C@H](OCC3=CC=CC=C3)[C@@H](COCC4=CC=CC=C4)O1")

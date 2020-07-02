d739fe761bfebf895695d79f180e7bb260103c01
Gabriel <62520394+dickeygh@users.noreply.github.com>
2020-07-02 15:44:00 -0400

    Removed store() function from models.py

--- core/models.py
+++ core/models.py
@@ -36,7 +36,6 @@ class MlModel:  # TODO update documentation here
 
         self.algorithm = algorithm
         self.dataset = dataset
-
         multi_label_classification_datasets = ['sider.csv', 'clintox.csv'] # List of multi-label classification data sets
         # Sets self.task_type based on which dataset is being used.
         if self.dataset in cds:
@@ -110,95 +109,3 @@ class MlModel:  # TODO update documentation here
         self.pva_graph()
         # TODO Make classification graphing function
 
-    def store(self):
-        """  Organize and store model inputs and outputs.  """
-
-        # create model file name
-        csvfile = ''.join("%s.csv" % self.run_name)
-
-        try:
-            os.mkdir(self.run_name)
-        except OSError as e:
-            pass
-
-        # create dictionary of attributes
-        att = dict(vars(self))  # makes copy so does not affect original attributes
-        del att['data']  # do not want DF in dict
-        # del att['smiles']  # do not want series in dict
-        # del att['graphM']  # do not want graph object
-        # del att['stats']  # will unpack and add on
-        # del att['pvaM']  # do not want DF in dict
-        del att['run_name']
-        try:
-            del att['varimp']  # don't need variable importance in our machine learning results record
-            del att['impgraph']  # Don't need a graph object in our csv
-        except KeyError:
-            pass
-        # del att['impgraph']
-        # att.update(self.stats)
-        # att.update(self.varimp)
-        # Write contents of attributes dictionary to a CSV
-        with open(csvfile, 'w') as f:  # Just use 'w' mode in Python 3.x
-            w = csv.DictWriter(f, att.keys())
-            w.writeheader()
-            w.writerow(att)
-            f.close()
-
-        # Path to output directory
-        output_directory = ''.join(['%s\output' % os.getcwd()])
-        # Copy csv file to ouput directory
-        shutil.copy(csvfile, output_directory)
-
-        # save data frames
-        self.data.to_csv(''.join("%s_data.csv" % self.run_name))
-        # self.pvaM.to_csv(''.join("%s_predictions.csv" % self.run_name))
-
-        # save graphs
-        # self.graphM.savefig(''.join("%s_PvAM" % self.run_name), transparent=True)
-        # if self.algorithm in ['rf', 'gdb'] and self.feat_meth == [0]:
-        #     self.impgraph.savefig(''.join("%s_impgraph" % self.run_name), transparent=True)
-        #     self.impgraph.close()
-        # else:
-        #     pass
-        # self.graphM.close()  # close to conserve memory when running many models.
-        # self.graph.savefig(name+'PvA')
-
-        # make folders for each run
-        # put output files into new folder
-        filesp = ''.join(['move ./', self.run_name, '* ', self.run_name, '/'])  # move for Windows system
-        # filesp = ''.join(['mv ./', self.run_name, '* ', self.run_name, '/'])  # mv for Linux system
-        subprocess.Popen(filesp, shell=True, stdout=subprocess.PIPE)  # run bash command
-
-        movepkl = ''.join(['move ./', '.pkl', '* ', self.run_name, '/'])  # move for Windows system
-        # movepkl = ''.join(['mv ./', '.pkl', '* ', self.run_name, '/']) # mv for Linux system
-        subprocess.Popen(movepkl, shell=True, stdout=subprocess.PIPE)  # run bash command
-
-        # Move folder to output/
-        # when testing using code below, need ../output/ because it will run from core.
-        # when running from main.py at root, no ../ needed.
-        # movesp = 'move ./' + run_name + ' output/'
-        #
-        # subprocess.Popen(movesp, shell=True, stdout=subprocess.PIPE)  # run bash command
-
-# This section is for troubleshooting and should be commented out when finished testing
-
-# # change active directory
-# with misc.cd('/dataFiles/'):
-#     print('Now in:', os.getcwd())
-#     print('Initializing model...', end=' ', flush=True)
-#     # initiate model class with algorithm, dataset and target
-#     model1 = MlModel('rf', 'ESOL.csv', 'water-sol', 'regression', tune=True, cv=3, opt_iter=25)
-#     print('done.')
-#
-# # # featurize data with rdkit2d
-# model1.featurize([0])
-# model1.data_split(val=0.0)
-# model1.run()
-# model1.analyze()
-# model1.export_json()
-# import pprint
-
-
-# print(dict(vars(model1)).keys())
-# pprint.pprint(dict(vars(model1)))
-# # print(model1.feat_meth)

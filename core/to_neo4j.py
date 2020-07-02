ebd65bc3b6d57126f2f712cf2d63c967665e7a27
qle <quangle270799@gmail.com>
2020-07-02 18:28:49 -0400

    change way of making nodes

--- core/to_neo4j.py
+++ core/to_neo4j.py
@@ -6,7 +6,8 @@ instance
 
 from core import ogm_class, models, misc
 from py2neo import Graph, Relationship, Node
-import pathlib, os
+from sklearn.metrics import mean_squared_error, r2_score
+import numpy as np
 # Connect to Neo4j Destop.
 g = Graph("bolt://localhost:7687", user="neo4j", password="1234")
 
@@ -18,6 +19,8 @@ CALL apoc.refactor.mergeNodes(nodelist) YIELD node
 RETURN node
 
 """
+
+
 
 
 def algorithm(self):
@@ -43,15 +46,36 @@ def features(self):
         g.create(feature_name)
 
 
-def model(self):
+def to_neo4j(self):
     """"""
-    model = ogm_class.MlModel()
-    model.name = self.run_name
-    model.feat_time = self.feat_time
-    model.date = self.date
-    model.train_time = self.tune_time
+    tuning_algorithm = Node("TuningAlg", name="TuningAlg", algorithm="BayesianOptimizer", num_cv=self.cv_folds,
+                            tuneTime=self.tune_time, delta=self.cp_delta, n_best=self.cp_n_best, steps=self.opt_iter)
+    g.create(tuning_algorithm)
 
+    model = Node("MLModel", name=self.run_name, feat_time=self.feat_time, date=self.date, train_time=self.tune_time,
+                 test_time=self.predictions_stats["time_avg"])
+    g.create(model)
 
+    feature_list = Node("FeatureList", name="FeatureList", num=len(self.feature_list))
+    g.create(feature_list)
+
+    trainsing_size = len(self.target_array) * self.train_percent
+    training_set = Node("TrainingSet", size=trainsing_size, name="TrainingSet")
+    g.create(training_set)
+
+    dataset = Node("DataSet", name="Dataset", source="Moleculenet", data=self.dataset, measurement=self.target_name)
+    g.create(dataset)
+
+    randomsplit = Node("RamdomSplit", name="RandomSplit", test_percent=self.test_percent,
+                       train_percent=self.train_percent, random_seed=self.random_seed)
+    g.create(randomsplit)
+
+    pva = self.predictions
+    r2 = r2_score(pva['actual'], pva['pred_avg'])
+    mse = mean_squared_error(pva['actual'], pva['pred_avg'])
+    rmse = np.sqrt(mean_squared_error(pva['actual'], pva['pred_avg']))
+
+    testset = Node("TestSet", name="TestSet", RMSE=rmse, mse=mse, r2=r2)
 
 
 

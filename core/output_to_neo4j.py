c37759ee6cf6476ad9111583a9b130adf8348cd7
qle <quangle270799@gmail.com>
2020-07-10 09:40:42 -0400

    add documentation

--- core/output_to_neo4j.py
+++ core/output_to_neo4j.py
@@ -1,5 +1,6 @@
 """
-Objective: Create Neo4j graphs from files inside of "output"
+Objective: Contains the function "output_to_neo4j" which is the function that imports data from output folders into
+            Neo4j based in our ontology
 """
 import pandas as pd
 import os
@@ -9,31 +10,40 @@ from core import misc
 import json
 from core import output_nodes_to_neo4j, output_rel_to_neo4j, prep_from_outputs
 
-# TODO: Add documentation
 
 g = Graph("bolt://localhost:7687", user="neo4j", password="1234")
 
 
 def file_count():
     """
-
-    :return:
+    Objective: Count the number of zip files in the "output" folder.
+    Intent: I want to use the "yield" command to return dataframe in a for loop later on, which will create generator
+            objects. Having the file counts me the ability to move to the next variable stored in the generator object.
+    More info: https://stackoverflow.com/questions/231767/what-does-the-yield-keyword-do
+    :return: Number of zip files in the output folder
     """
     # print(hello)
     with misc.cd('../output/'):  # Access output
         print('Now in:', os.getcwd())
         for roots, dirs, files in os.walk(os.getcwd()):
-            file_count = 0
-            json_list = []
-            csv_list = []
+            files_count = 0
             for f in files:
                 if f.endswith('.zip'):
-                    file_count += 1
-            return file_count
+                    files_count += 1
+            print(f"You have {files_count} zip files in your output folder")
+            return files_count
 
 
 def get_file(file_string):
-    """"""
+    """
+    Objective: Yield dataframes in a for loop for memory efficiency.
+    Intent: I want this function to access every zip files in the "output" folder and turn the necessary files into
+            dataframe. The key here is to do it as fast and memory efficient as possible. The Yield command ... yields
+            a generator object that is much lighter by nature than other storage data type like lists or tuples. It will
+            bloat up memory if I return a list or a tuple of 100s of dataframes and bottleneck the process.
+    :param file_string: A common substring in a file name in our zip files
+    :return: 3 dataframe from _attributes.json, _data.csv and _predictions.csv
+    """
     with misc.cd('../output/'):  # Access output
         for roots, dirs, files in os.walk(os.getcwd()):
             for f in files:
@@ -48,18 +58,23 @@ def get_file(file_string):
                                     df_from_attributes = pd.json_normalize(data)  # Normalizing json data into dataframe
                                 yield df_from_attributes
                             if file_string in file and file_string == "_data.csv":
-                                df_from_data = pd.read_csv(zip.open(file))
+                                df_from_data = pd.read_csv(zip.open(file))  # Open dataframe in zip
                                 yield df_from_data
                             if file_string in file and file_string == "_predictions.csv":
-                                df_from_predictions = pd.read_csv(zip.open(file))
+                                df_from_predictions = pd.read_csv(zip.open(file))  # Open dataframe in zip
                                 yield df_from_predictions
 
 
 def output_to_neo4j():
-    """"""
-    json_generator = get_file('_attributes.json')
-    data_csv_generator = get_file('_data.csv')
-    predictions_csv_generator = get_file("_predictions.csv")
+    """
+    Objective: Import data from output files into Neo4j based on our ontology.
+    Intent: This want this function to be the final command that will put data into Neo4j. Should not contain anything
+            that doesn't help accomplish that goal.
+    :return:
+    """
+    json_generator = get_file('_attributes.json')  # attribute file string
+    data_csv_generator = get_file('_data.csv')  # data file string
+    predictions_csv_generator = get_file("_predictions.csv")  # predictions file string
     for i in range(file_count()):
         # Json dataframe
         df_from_attributes = next(json_generator)
@@ -67,11 +82,10 @@ def output_to_neo4j():
         df_from_data = next(data_csv_generator)
         df_from_predictions = next(predictions_csv_generator)
         prep = prep_from_outputs.Prep(df_from_attributes, df_from_predictions, df_from_data)
-        # print(prep.n_test)
+        # print(prep.num_feature_list)
         output_nodes_to_neo4j.nodes(prep)
         output_rel_to_neo4j.relationships(prep)
 
+
+# You can uncomment this to run the process of importing data from output files into Neo4j based on our ontology
 # output_to_neo4j()
-
-
-

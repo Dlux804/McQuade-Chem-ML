062106c30a25938de5f7c1d47978b263cbce5acb
qle <quangle270799@gmail.com>
2020-07-10 10:43:35 -0400

    add documentations

--- core/prep_from_outputs.py
+++ core/prep_from_outputs.py
@@ -6,35 +6,41 @@ from core import fragments
 import numpy as np
 from sklearn.metrics import mean_squared_error, r2_score
 
-# TODO: Add documentation
 
-
-def split_molecules(dataframe, split):
+def remove_whitespace(string):
     """
-
-    :param dataframe:
-    :param split:
+    Objective: Remove whitespaces from string variables
+    Intent: Because I gather data from output files based on column names (the data is contained in dataframes
+            at this point), they are Pandas Series object. When I convert them into string and remove the index label
+            using "to_string(index=False)", there are whitespaces in the string variable. Therefore, I needed to create
+            this function to get rid of the whitespaces. I should have found a more clever way to obtain data from
+            dataframes instead of doing all this.
+    :param string: A string. What did you expect it to be???
     :return:
     """
-    split = ''.join([split[0].lower(), split[1:]])
-    df = dataframe.loc[dataframe['in_set'] == split]
-    df = df.reset_index(drop=True)
-    df = df.drop(['Unnamed: 0'], axis=1)
-    molecules = fragments.canonical_smiles(list(df['smiles']))
-    return molecules
+    return "".join(string.split())
 
 
 class Prep:
     """
-    Class containing all the data needed for creating nodes and relationships from output files
+    Objective: Class containing all the data needed for creating nodes and relationships from output files
+    Intent: I wanted to find a way to contain all the necessary data in a more tidy manner and this is the only thing
+            that came to my mind. I didn't put comments explaining each of them since I think they are pretty
+            self-explanatory.
+    Note: I have to put "num_feature_list" (number of features) in a list because that's the only way
+            I can obtain the number of features in a clean manner. If I use "len" on the Series object, it will only
+            return 1 instead the correct number of features. Turning the Series object into numpy array also return
+            1 for "len". Seems like "len" is counting the number of element in a Pandas column (which is one) instead of
+            the number of element contained in the Series object (the number of features). While I can add a for loop
+            in to get the number inside the list, it is not very clean.
     """
     def __init__(self, df_from_attributes, df_from_predictions, df_from_data):
         self.df_from_data = df_from_data
         self.df_from_attributes = df_from_attributes
-        self.run_name = df_from_attributes['run_name'].to_string(index=False)
-        self.algorithm = df_from_attributes['algorithm'].to_string(index=False)
+        self.run_name = remove_whitespace(df_from_attributes['run_name'].to_string(index=False))
+        self.algorithm = remove_whitespace(df_from_attributes['algorithm'].to_string(index=False))
         self.feat_method_name = df_from_attributes['feat_method_name'].tolist()
-        self.tuned = df_from_attributes['tuned'].to_string(index=False)
+        self.tuned = remove_whitespace(df_from_attributes['tuned'].to_string(index=False))
         self.cv_folds = int(df_from_attributes['cv_folds'])
         self.n_test = int(df_from_attributes['n_test'])
         self.tune_time = float(df_from_attributes['tune_time'])
@@ -42,13 +48,13 @@ class Prep:
         self.cp_n_best = int(df_from_attributes['cp_n_best'])
         self.opt_iter = int(df_from_attributes['opt_iter'])
         self.feat_time = float(df_from_attributes['feat_time'])
-        self.date = df_from_attributes['date'].to_string(float_format=True, index=False)
+        self.date = remove_whitespace(df_from_attributes['date'].to_string(float_format=True, index=False))
         self.tune_time = float(df_from_attributes['tune_time'])
         self.test_time = float(df_from_attributes["predictions_stats.time_avg"])
-        self.num_feature_list = len(list(df_from_attributes['feature_list']))
+        self.num_feature_list = [len(i) for i in list(df_from_attributes['feature_list'])]  # number of features
         self.n_train = int(df_from_attributes['n_train'])
-        self.dataset_str = df_from_attributes['dataset'].to_string(float_format=True, index=False)
-        self.target_name = df_from_attributes['target_name'].to_string(float_format=True, index=False)
+        self.dataset_str = remove_whitespace(df_from_attributes['dataset'].to_string(float_format=True, index=False))
+        self.target_name = remove_whitespace(df_from_attributes['target_name'].to_string(float_format=True, index=False))
         self.test_percent = float(df_from_attributes['test_percent'])
         self.train_percent = float(df_from_attributes['train_percent'])
         self.random_seed = int(df_from_attributes['random_seed'])

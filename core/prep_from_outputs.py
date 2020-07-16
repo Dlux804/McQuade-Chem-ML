8281d469ecdd1b27c2781e30c634ec5e65ee68a5
qle <quangle270799@gmail.com>
2020-07-16 13:19:37 -0400

    adapt for both outputs and pipeline

--- core/prep_from_outputs.py
+++ core/prep_from_outputs.py
@@ -2,10 +2,12 @@
 Objective: This script stores all data from output files. The data will then be used to create nodes and
 relationships based on our ontology to Neo4j
 """
-from core import fragments
+from core import fragments, nodes_to_neo4j, rel_to_neo4j
 import numpy as np
 from sklearn.metrics import mean_squared_error, r2_score
 import pandas as pd
+import re
+from ast import literal_eval
 
 
 def remove_whitespace(string):
@@ -28,7 +30,6 @@ def split_molecules(df_from_data, split):
     return df_molecules
 
 
-
 class Prep:
     """
     Objective: Class containing all the data needed for creating nodes and relationships from output files
@@ -43,28 +44,24 @@ class Prep:
             in to get the number inside the list, it is not very clean.
     """
     def __init__(self, df_from_attributes, df_from_predictions, df_from_data):
-        self.df_from_data = df_from_data.drop(['Unnamed: 0'], axis=1)
-        self.df_from_attributes = df_from_attributes
+        self.data = df_from_data.drop(['Unnamed: 0'], axis=1)
+        df_from_attributes = df_from_attributes.rename(columns=lambda x: re.sub("predictions_stats.", "", x))
+        self.params = df_from_attributes.filter(regex="params.", axis=1).rename(columns=lambda x: re.sub("params.", "", x))
+        self.predictions_stats = df_from_attributes.loc[:, 'r2_raw':'time_std']
         self.run_name = remove_whitespace(df_from_attributes['run_name'].to_string(index=False))
         self.algorithm = remove_whitespace(df_from_attributes['algorithm'].to_string(index=False))
         self.feat_method_name = df_from_attributes['feat_method_name'].tolist()
-        self.tuned = remove_whitespace(df_from_attributes['tuned'].to_string(index=False))
-        self.cv_folds = int(df_from_attributes['cv_folds'])
+        self.tuned = literal_eval(remove_whitespace(df_from_attributes['tuned'].to_string(index=False)))
         self.n_test = int(df_from_attributes['n_test'])
-        self.tune_time = float(df_from_attributes['tune_time'])
-        self.cp_delta = int(df_from_attributes['cp_delta'])
-        self.cp_n_best = int(df_from_attributes['cp_n_best'])
-        self.opt_iter = int(df_from_attributes['opt_iter'])
         self.feat_time = float(df_from_attributes['feat_time'])
         self.date = remove_whitespace(df_from_attributes['date'].to_string(float_format=True, index=False))
-        self.tune_time = float(df_from_attributes['tune_time'])
-        self.test_time = float(df_from_attributes["predictions_stats.time_avg"])
-        self.num_feature_list = [len(i) for i in list(df_from_attributes['feature_list'])]  # number of features
+        self.test_time = float(df_from_attributes["time_avg"])
+        self.feature_list = int(df_from_attributes['feature_length'])
         self.n_train = int(df_from_attributes['n_train'])
-        self.dataset_str = remove_whitespace(df_from_attributes['dataset'].to_string(float_format=True, index=False))
+        self.dataset = remove_whitespace(df_from_attributes['dataset'].to_string(float_format=True, index=False))
         self.target_name = remove_whitespace(df_from_attributes['target_name'].to_string(float_format=True, index=False))
-        self.test_percent = float(df_from_attributes['test_percent'])
         self.train_percent = float(df_from_attributes['train_percent'])
+        self.test_percent = float(df_from_attributes['test_percent'])
         self.random_seed = int(df_from_attributes['random_seed'])
         self.val_percent = float(df_from_attributes['val_percent'])
         self.params_list = list(df_from_attributes.filter(regex='params.'))
@@ -73,7 +70,8 @@ class Prep:
         self.n_val = int(df_from_attributes['n_val'])
         self.rdkit2d_features = df_from_data.loc[:, 'smiles':'qed']
         self.features_col = list(self.rdkit2d_features.columns)
-        pva = df_from_predictions
+        self.predictions = df_from_predictions
+        pva = self.predictions
         self.r2 = r2_score(pva['actual'], pva['pred_avg'])  # r2 values
         self.mse = mean_squared_error(pva['actual'], pva['pred_avg'])  # mse values
         self.rmse = np.sqrt(mean_squared_error(pva['actual'], pva['pred_avg']))  # rmse values
@@ -84,3 +82,20 @@ class Prep:
         self.train_molecules = split_molecules(df_from_data, 'train')
         self.test_molecules = split_molecules(df_from_data, 'test')
         self.val_molecules = split_molecules(df_from_data, 'val')
+        self.feature_length = int(df_from_attributes['feature_length'])
+        if self.tuned:
+            self.cv_folds = int(df_from_attributes['cv_folds'])
+            self.tune_time = float(df_from_attributes['tune_time'])
+            self.cp_delta = int(df_from_attributes['cp_delta'])
+            self.cp_n_best = int(df_from_attributes['cp_n_best'])
+            self.opt_iter = int(df_from_attributes['opt_iter'])
+        else:
+            self.cv_folds = 0
+            self.tune_time = 0
+            self.cp_delta = 0
+            self.cp_n_best = 0
+            self.opt_iter = 0
+
+    def to_neo4j(self):
+        nodes_to_neo4j.nodes(self)
+        rel_to_neo4j.relationships(self)

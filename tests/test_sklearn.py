f2eda25f0cb939b79f4cdd62a86270828cfb93c2
qle <quangle270799@gmail.com>
2020-07-21 11:05:11 -0400

    test circleci

--- tests/test_sklearn.py
+++ tests/test_sklearn.py
@@ -4,7 +4,6 @@ Objective: Test to see if the pipeline runs sklearn methods for tuned and un-tun
 
 from core import models
 import os
-from tests import ml_assert
 
 # Test for almost all models instead of knn. With a small dataset, knn throws a fit
 algorithm_list = ['ada', 'svr', 'rf', 'gdb']
@@ -20,13 +19,16 @@ def test_sklearn_no_tuned():
         model1.reg()
         model1.run()
         model1.analyze()
-        assert os.path.isfile(''.join([model1.run_name, '_PVA.png']))
-        os.remove(''.join([model1.run_name, '_PVA.png']))
-        assert float(model1.predictions_stats['mse_avg']) > 0.5
-        assert float(model1.predictions_stats['r2_avg']) < 0.8
-        if algorithm in ['rf', 'gdb']:
+        model1.pickle_model()
+        assert os.path.isfile(''.join([model1.run_name, '_PVA.png']))  # Check for PVA graphs
+        assert os.path.isfile(''.join([model1.run_name, '.pkl']))
+        os.remove(''.join([model1.run_name, '_PVA.png']))  # Delete PVA graphs
+        os.remove(''.join([model1.run_name, '.pkl']))
+        assert float(model1.predictions_stats['mse_avg']) > 0.5  # Check for mse value
+        assert float(model1.predictions_stats['r2_avg']) < 0.8  # Check for r2_avg value
+        if algorithm in ['rf', 'gdb']:   # Check for variable importance graphs
             assert os.path.isfile(''.join([model1.run_name, '_importance-graph.png']))
-            os.remove(''.join([model1.run_name, '_importance-graph.png']))
+            os.remove(''.join([model1.run_name, '_importance-graph.png']))  # Remove graph
 
 
 def test_sklearn_tuned():
@@ -39,8 +41,11 @@ def test_sklearn_tuned():
         model1.reg()
         model1.run()
         model1.analyze()
+        model1.pickle_model()
         assert os.path.isfile(''.join([model1.run_name, '_PVA.png']))
         assert os.path.isfile(''.join([model1.run_name, '_checkpoint.pkl']))
+        assert os.path.isfile(''.join([model1.run_name, '.pkl']))
+        os.remove(''.join([model1.run_name, '.pkl']))
         os.remove(''.join([model1.run_name, '_PVA.png']))
         os.remove(''.join([model1.run_name, '_checkpoint.pkl']))
         assert float(model1.predictions_stats['mse_avg']) > 0.5

6a7b49b675fd17cccf4ede8bfb4cb44760740194
qle <quangle270799@gmail.com>
2020-07-21 14:51:06 -0400

    commit to make branch

---
+++ tests/test_storage.py
@@ -0,0 +1,13 @@
+"""
+Objective: Test functionality of essential functions in storage.py
+"""
+
+from core import models
+from core import storage
+import mock
+
+
+def test_store():
+    model1 = models.MlModel(algorithm='rf', dataset='Lipo-short.csv', target='exp', feat_meth=[0],
+                            tune=False, cv=2, opt_iter=2)
+    model1.store()

a07b6b109ce36aae8ca47ee7a139c2ce30e71c3f
qle <quangle270799@gmail.com>
2020-07-21 16:55:11 -0400

    add more h5 file assertion

--- tests/test_keras.py
+++ tests/test_keras.py
@@ -3,30 +3,40 @@ Objective: Test to see if the pipeline runs keras methods for tuned and un-tuned
 """
 from core import models
 import os
-
-
-filename = os.path.join(os.path.dirname(__file__), 'Lipo-short.csv')
+from core.storage import misc
 
 
 def test_keras_no_tuned():
     """"""
-    model1 = models.MlModel(algorithm='nn', dataset=filename, target='exp', feat_meth=[0],
-                            tune=False, cv=2, opt_iter=2)
+    model1 = models.MlModel(algorithm='nn', dataset='Lipo-short.csv', target='exp', feat_meth=[0], tune=False, cv=2,
+                                opt_iter=2)
     model1.featurize()
     model1.data_split(val=0.1)
     model1.reg()
     model1.run()
-    assert float(model1.predictions_stats['mse_avg']) > 0.5
-    assert float(model1.predictions_stats['r2_avg']) < 0.8
+    model1.analyze()
+    assert os.path.isfile(''.join([model1.run_name, '_PVA.png']))  # Check for PVA graphs
+    assert os.path.isfile(''.join([model1.run_name, '.h5']))  # Check for PVA graphs
+    os.remove(''.join([model1.run_name, '.h5']))
+    os.remove(''.join([model1.run_name, '_PVA.png']))  # Delete PVA graphs
+    assert float(model1.predictions_stats['mse_avg']) > 0.5  # Check for mse value
+    assert float(model1.predictions_stats['r2_avg']) < 0.8  # Check for r2_avg value
 
 
 def test_keras_tuned():
     """"""
-    model1 = models.MlModel(algorithm='nn', dataset=filename, target='exp', feat_meth=[0],
-                                tune=True, cv=2, opt_iter=2)
+    model1 = models.MlModel(algorithm='nn', dataset='Lipo-short.csv', target='exp', feat_meth=[0], tune=True, cv=2,
+                            opt_iter=2)
     model1.featurize()
     model1.data_split(val=0.1)
     model1.reg()
     model1.run()
-    assert float(model1.predictions_stats['mse_avg']) > 0.5
-    assert float(model1.predictions_stats['r2_avg']) < 0.8
+    model1.analyze()
+    assert os.path.isfile(''.join([model1.run_name, '_PVA.png']))  # Check for PVA graphs
+    assert os.path.isfile(''.join([model1.run_name, '.h5']))  # Check for PVA graphs
+    assert os.path.isfile(''.join([model1.run_name, '_checkpoint.pkl']))
+    os.remove(''.join([model1.run_name, '_checkpoint.pkl']))
+    os.remove(''.join([model1.run_name, '.h5']))
+    os.remove(''.join([model1.run_name, '_PVA.png']))  # Delete PVA graphs
+    assert float(model1.predictions_stats['mse_avg']) > 0.5  # Check for mse value
+    assert float(model1.predictions_stats['r2_avg']) < 0.8  # Check for r2_avg value

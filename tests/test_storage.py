e218cf85cc71c7ec8223f5872079028471b8980f
qle <quangle270799@gmail.com>
2020-07-22 12:01:52 -0400

    test circleci

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -3,12 +3,17 @@ Objective: Test functionality of essential functions in storage.py
 """
 from core.storage import misc
 from core import models
-from core import storage
-import mock
+from core.storage import storage
+import pytest
 import os
+import pandas as pd
 
 
 def test_store():
-    model1 = models.MlModel(algorithm='rf', dataset='Lipo-short.csv', target='exp', feat_meth=[0],
-                            tune=False, cv=2, opt_iter=2)
-    model1.store()
+    """"""
+    with misc.cd('../tests'):
+        model1 = models.MlModel(algorithm='rf', dataset='Lipo-short.csv', target='exp', feat_meth=[0], tune=False, cv=2,
+                                opt_iter=2)
+
+
+

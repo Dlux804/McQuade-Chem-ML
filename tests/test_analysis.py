000d319ccb9cb117e6867af79a07199eb7e41be3
qle <quangle270799@gmail.com>
2020-07-25 16:26:51 -0400

    test analysis.py's functions

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,36 +1,28 @@
 """
 Objective: Test functions in analysis.py
 """
-
+from core.storage import misc
 import pytest
 import os, glob
-from tests.fixtures.model_fixture import __sklearn_untuned__, __sklearn_tuned__, __assert_results__
+from tests.model_fixture import __run_all__, delete_files
 
 tree_algorithm = ['rf', 'gdb']
 
 
 @pytest.mark.parametrize('algorithm', tree_algorithm)
-@pytest.mark.parametrize('data, exp', [('Lipo-short.csv', 'exp')])
-def test_var_importance(__sklearn_tuned__):
-    model1 = __sklearn_tuned__
-    model1.featurize()
-    model1.data_split(val=0.1)
-    model1.reg()
-    model1.run()
-    model1.analyze()
+@pytest.mark.parametrize('data, exp, tuned, delete, directory', [('Lipo-short.csv', 'exp', False, False, True)])
+def test_var_importance(__run_all__):
+    model1 = __run_all__
+
     assert os.path.isfile(''.join([model1.run_name, '_importance-graph.png']))
-    list(map(os.remove, glob.glob('*%s*' % model1.run_name)))
+    delete_files(model1.run_name)
 
 
-@pytest.mark.parametrize('algorithm, data, exp', [('rf', 'Lipo-short.csv', 'exp')])
-def test_var_importance(__sklearn_untuned__):
-    model1 = __sklearn_untuned__
-    model1.featurize()
-    model1.data_split(val=0.1)
-    model1.reg()
-    model1.run()
-    model1.analyze()
+@pytest.mark.parametrize('algorithm, data, exp, tuned, delete, directory', [('rf', 'Lipo-short.csv', 'exp', False,
+                                                                             False, True)])
+def test_pva_graph(__run_all__):
+    model1 = __run_all__
     assert os.path.isfile(''.join([model1.run_name, '_PVA.png']))  # Check for PVA graphs
-    list(map(os.remove, glob.glob('*%s*' % model1.run_name)))
+    delete_files(model1.run_name)
 
 

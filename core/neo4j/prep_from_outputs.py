3b4fe346da03bf34df7a5e63c532926999ccf420
qle <quangle270799@gmail.com>
2020-08-01 15:09:07 -0400

    fix target array error

--- core/neo4j/prep_from_outputs.py
+++ core/neo4j/prep_from_outputs.py
@@ -55,7 +55,7 @@ class Prep:
         self.random_seed = int(df_from_attributes['random_seed'].values[0])
         self.params_list = list(df_from_attributes.filter(regex='params.'))
         self.canonical_smiles = fragments.canonical_smiles(list(df_from_data['smiles']))
-        self.target_array = list(df_from_data['target'])
+        self.target_array = list(df_from_data[self.target_name])
         self.n_val = int(df_from_attributes['n_val'])
         self.predictions = df_from_predictions
         pva = self.predictions
@@ -71,10 +71,6 @@ class Prep:
         self.val_molecules = split_molecules(df_from_data, 'val')
         self.feat_meth = df_from_attributes['feat_meth'].values[0]
         self.tune_algorithm_name = df_from_attributes['tune_algorithm_name'].values[0]
-        if "rdkit2d" in self.feat_method_name:
-            self.rdkit2d_features = df_from_data.loc[:, 'smiles':'qed']
-        else:
-            pass
         if self.tuned:
             self.cv_folds = int(df_from_attributes['cv_folds'].values[0])
             self.tune_time = float(df_from_attributes['tune_time'].values[0])
@@ -96,4 +92,4 @@ class Prep:
         Graph(self.neo4j_params["port"], username=self.neo4j_params["username"],
               password=self.neo4j_params["password"])  # Test connection to Neo4j
         nodes_to_neo4j.nodes(self)
-        rel_to_neo4j.relationships(self)
+        # rel_to_neo4j.relationships(self)

0a151c505a71f25fd810bdb7e3e59a4210d3c979
qle <quangle270799@gmail.com>
2020-08-03 19:57:06 -0400

    adjust for testing

--- main.py
+++ main.py
@@ -135,8 +135,8 @@ def single_model():
         print('Now in:', os.getcwd())
         print('Initializing model...', end=' ', flush=True)
         # initiate model class with algorithm, dataset and target
-        model1 = models.MlModel(algorithm='rf', dataset='ESOL.csv', target='water-sol', feat_meth=[0],
-                                tune=True, cv=2, opt_iter=2)
+        model1 = models.MlModel(algorithm='svr', dataset='logP14k.csv', target='Kow', feat_meth=[0],
+                                tune=False, cv=2, opt_iter=2)
 
         print('done.')
         print('Model Type:', model1.algorithm)
@@ -154,9 +154,8 @@ def single_model():
             model1.pickle_model()
         model1.store()
         model1.org_files(zip_only=True)
-        model1.QsarDB_export(zip_output=True)
-        model1.to_neo4j()
-
+        # model1.QsarDB_export(zip_output=True)
+        model1.to_neo4j(port="bolt://localhost:7687", username="neo4j", password="password")
 
 
 def example_run_with_mysql_and_neo4j():

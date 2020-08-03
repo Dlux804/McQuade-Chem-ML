2bb441a33040b8187ddd8fa9e99fe55254d00f0b
Gabriel <62520394+dickeygh@users.noreply.github.com>
2020-08-03 15:35:17 -0400

    a

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # TODO: Make main function that asks user what models they would like to initiate
 
-from core import models
+from core import models, Get_Classification
 from core.storage.misc import cd
 from core.storage.storage import pickle_model, unpickle_model
 import os
@@ -32,22 +32,10 @@ def main():
     # All data sets in dict
     targets = None
     sets = {
-#        'BBBP.csv': 'p_np',
-        'sider.csv': ['Hepatobiliary disorders', 'Metabolism and nutrition disorders', 'Product issues',
-                   'Eye disorders', 'Investigations', 'Musculoskeletal and connective tissue disorders',
-                   'Gastrointestinal disorders',
-                   'Social circumstances', 'Immune system disorders', 'Reproductive system and breast disorders',
-                   'Neoplasms benign, malignant and unspecified',
-                   'General disorders and administration site conditions', 'Endocrine disorders',
-                   'Surgical and medical procedures', 'Vascular disorders', 'Blood and lymphatic system disorders',
-                   'Skin and subcutaneous tissue disorders', 'Congenital, familial and genetic disorders',
-                   'Infections and infestations', 'Respiratory, thoracic and mediastinal disorders',
-                   'Psychiatric disorders', 'Renal and urinary disorders',
-                   'Pregnancy, puerperium and perinatal conditions', 'Ear and labyrinth disorders',
-                   'Cardiac disorders',
-                   'Nervous system disorders', 'Injury, poisoning and procedural complications'],
-        'clintox.csv': ['FDA_APPROVED', 'CT_TOX'],
-#        'bace.csv': 'Class',
+        'BBBP.csv': targets,
+#        'sider.csv': targets,
+#        'clintox.csv': targets,
+        'bace.csv': targets,
         # 'ESOL.csv': 'water-sol',
         # 'Lipophilicity-ID.csv': 'exp',
         # 'water-energy.csv': 'expt',
@@ -77,6 +65,8 @@ def main():
                   [5], [6]] # Use this line to select specific featurizations
         for method in feats:  # loop over the featurization methods
             for data, target in sets.items(): # loop over dataset dictionary
+                if data in ['BBBP.csv', 'sider.csv', 'clintox.csv', 'bace.csv']:
+                    target = Get_Classification.get_classification_targets(data)
 
                 if data in ['sider.csv', 'clintox.csv'] and alg == 'svc':
                     pass

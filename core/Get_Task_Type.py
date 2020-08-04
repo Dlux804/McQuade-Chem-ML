6b5dc5ad912a76256c5e7b81cc7b345d82f5cc53
Gabriel <62520394+dickeygh@users.noreply.github.com>
2020-08-04 13:52:19 -0400

    Added Get_Task_Type.py

---
+++ core/Get_Task_Type.py
@@ -0,0 +1,14 @@
+def Get_Task_Type_1(data, alg):
+    if data in ['sider.csv', 'clintox.csv'] and alg == 'svc':
+        checker = 0
+    elif data in ['BBBP.csv', 'sider.csv', 'clintox.csv', 'bace.csv'] and alg in ['ada', 'svr', 'gdb', 'nn',
+                                                                                  'knn']:
+        checker = 0
+    elif data in ['ESOL.csv', 'Lipophilicity-ID.csv', 'water-energy.csv', 'logP14k.csv', 'jak2_pic50.csv'] and alg in [
+        'svc', 'knc']:
+        checker = 0
+
+    else:
+        checker = 1
+
+    return checker

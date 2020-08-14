3357d1301570834db1e3f19af409f6af7f7acb8d
qle <quangle270799@gmail.com>
2020-08-14 18:05:50 -0400

    remove test_sklearn.py

--- tests/test_sklearn.py
+++ tests/test_sklearn.py
@@ -5,23 +5,23 @@ import pytest
 import os, glob
 from tests.model_fixture import __run_all__, delete_files
 # Test for almost all models instead of knn. With a small dataset, knn throws a fit
-algorithm_list = ['ada', 'svm', 'rf', 'gdb']
-
-
-@pytest.mark.parametrize('algorithm', algorithm_list)
-@pytest.mark.parametrize('data, exp, tuned, directory', [('Lipo-short.csv', 'exp', False, True)])
-def test_sklearn_untuned(__run_all__):
-    """"""
-    # for algor in algorithm_list:
-    model1 = __run_all__
-
-
-@pytest.mark.parametrize('algorithm', algorithm_list)
-@pytest.mark.parametrize('data, exp, tuned, directory', [('Lipo-short.csv', 'exp', True, True)])
-def test_sklearn_tuned(__run_all__):
-    """"""
-    # for algor in algorithm_list:
-    model1 = __run_all__
+# algorithm_list = ['ada', 'svm', 'rf', 'gdb']
+#
+#
+# @pytest.mark.parametrize('algorithm', algorithm_list)
+# @pytest.mark.parametrize('data, exp, tuned, directory', [('Lipo-short.csv', 'exp', False, True)])
+# def test_sklearn_untuned(__run_all__):
+#     """"""
+#     # for algor in algorithm_list:
+#     model1 = __run_all__
+#
+#
+# @pytest.mark.parametrize('algorithm', algorithm_list)
+# @pytest.mark.parametrize('data, exp, tuned, directory', [('Lipo-short.csv', 'exp', True, True)])
+# def test_sklearn_tuned(__run_all__):
+#     """"""
+#     # for algor in algorithm_list:
+#     model1 = __run_all__
     # r2 = model1.predictions_stats['r2_avg']
     # mse = model1.predictions_stats['mse_avg']
     # rmse = model1.predictions_stats['rmse_avg']

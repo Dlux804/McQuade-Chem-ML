7330864fadf61233184230b16ee24196b105e93c
qle <quangle270799@gmail.com>
2020-09-09 16:51:26 -0400

    fix fixture

--- tests/model_fixture.py
+++ tests/model_fixture.py
@@ -92,7 +92,32 @@ def __run_all__(algorithm, data, exp, tuned, directory):
     model1.data_split(val=0.1)
     model1.reg()
     model1.run()
-    # model1.analyze()
+    model1.analyze()
+    return model1
+
+
+@pytest.fixture(scope="function")
+def __run_all__without_analyze(algorithm, data, exp, tuned, directory):
+    """
+    Objective: Run all
+    :param algorithm:
+    :param data:
+    :param exp:
+    :param tuned:
+    :param directory:
+    :return:
+    """
+    if directory:
+        with misc.cd('dataFiles/testdata'):
+            model1 = models.MlModel(algorithm=algorithm, dataset=data, target=exp, tune=tuned, feat_meth=[0], cv=2,
+                                    opt_iter=2)
+    else:
+        model1 = models.MlModel(algorithm=algorithm, dataset=data, target=exp, tune=tuned, feat_meth=[0], cv=2,
+                                opt_iter=2)
+    model1.featurize()
+    model1.data_split(val=0.1)
+    model1.reg()
+    model1.run()
     return model1
 
 
--- tests/test_keras.py
+++ tests/test_keras.py
@@ -3,18 +3,18 @@ Objective: Test to see if the pipeline runs keras methods for tuned and un-tuned
 """
 
 import pytest
-from tests.model_fixture import __run_all__
+from tests.model_fixture import __run_all__without_analyze
 
 
 @pytest.mark.parametrize('algorithm, data, exp, tuned, directory', [('nn', 'Lipo-short.csv', 'exp', False, True)])
-def test_sklearn_untuned(__run_all__):
+def test_sklearn_untuned(__run_all__without_analyze):
     """"""
     # for algor in algorithm_list:
-    model1 = __run_all__
+    model1 = __run_all__without_analyze
 
 
 @pytest.mark.parametrize('algorithm, data, exp, tuned, directory', [('nn', 'Lipo-short.csv', 'exp', True, True)])
-def test_sklearn_tuned(__run_all__):
+def test_sklearn_tuned(__run_all__without_analyze):
     """"""
     # for algor in algorithm_list:
-    model1 = __run_all__
+    model1 = __run_all__without_analyze

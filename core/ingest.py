71d4ddf37016d9bb9145bea9e5787710a0541f41
qle <quangle270799@gmail.com>
2020-11-16 09:31:22 -0500

    turn SMILES into Canonical SMILES

--- core/ingest.py
+++ core/ingest.py
@@ -3,13 +3,47 @@ import pandas as pd
 from rdkit import Chem
 
 
-def load_smiles(self, file, drop=True):
-    """ Find SMILES in CSV.  Return DataFrame and Series of SMILES.
+def canonical_smiles(df):
+    """
+    Objective: Create list of canonical SMILES from SMILES
+    Intent: While the SMILES in dataset from moleculenet.ai are all canonical, it is always good to be safe. I don't
+            know if I should add in a way to detect irregular SMILES and remove the rows that contains them in the
+            dataframe. However, that process should be carried out at the start of the pipeline instead of at the end.
+    :param smiles_list:
+    :return:
+    """
+    # print(df)
+    smiles = df['smiles']
+    con_smiles = []
+    for smile in smiles:
+        mol = Chem.MolFromSmiles(smile)
+        if mol is not None:
+            con_smiles.append(Chem.MolToSmiles(mol))
+        else:
+            con_smiles.append('bad_smiles')
+    # print(con_smiles)
+    # print(len(con_smiles))
+    df['smiles'] = con_smiles
+    df = df.loc[df['smiles'] != 'bad_smiles']
+
+    return df
+
+
+def load_smiles(self, file, target, drop=True):
+    """
+    Find SMILES in CSV.  Return DataFrame and Series of SMILES.
 
     Keyword Arguments
     drop -- Drop all other columns besides smiles and target. Default = True
+    :param self:
+    :param file:
+    :param drop:
+    :return csv: dataframe object
+    :return smiles_col: smiles column from csv
     """
+
     csv = pd.read_csv(file)
+
     for i in csv.head(0):
         try:
             pd.DataFrame(list(map(Chem.MolFromSmiles, csv[i])))
@@ -18,9 +52,12 @@ def load_smiles(self, file, drop=True):
         except Exception:
             pass
     # rename the column with SMILES to 'smiles'
+
     csv = csv.rename(columns={smiles_col.name: "smiles"})
+
+    csv = canonical_smiles(csv)
     if drop:  # drop all extra columns
-        csv = csv[['smiles', self.target_name]]
+        csv = csv[['smiles', target]]
 
     return csv, smiles_col
 
@@ -29,8 +66,10 @@ def resolveID(file, column):  # TODO Consider incorporation of this function in 
     """ Resolves chemical ID using cripy package from NCI.
     Accepts csv file path and name (as string) and string of column header to be resolved.
     Returns dataframe with added column containing smiles."""
-
-    df = pd.read_csv(file)  # read csv file
+    if isinstance(file, pd.core.frame.DataFrame):
+        df = file
+    elif isinstance(file, str):
+        df = pd.read_csv(file)  # read csv file
 
     # for i in df.head(0):  # look at all columns
     #     try:
@@ -49,21 +88,21 @@ def resolveID(file, column):  # TODO Consider incorporation of this function in 
     for i, row in enumerate(df.itertuples(), 1):  # iterate through dataframe
         c = row.Index
         id = df.loc[c, column]  # get cas number from df
-        print('Resolving', id)
+        # print('Resolving', id)
 
         # look up the CAS, convert to smiles
         df.loc[c, 'smiles'] = cirpy.resolve(id, 'smiles')  # store in df
 
         # provides output text
-        if df.loc[c, 'smiles'] == None:
-            print('No SMILES found')
-            print()
-
-        else:
-            print('smiles found :)')
-            print()
+        # if df.loc[c, 'smiles'] == None:
+        #     print('No SMILES found')
+        #     print()
+        #
+        # else:
+        #     print('smiles found :)')
+        #     print()
 
     # drop if smiles was not found
     df3 = df.dropna()
-    print(df3.head(5))
+    # print(df3.head(5))
     return df3

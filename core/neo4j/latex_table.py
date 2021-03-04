adb2639f13908866c468eff031139b7e28ecd177
adam <luxon.adam@gmail.com>
2021-03-04 14:27:20 -0500

    Lots of deletion.  Removing old or unused files for public presentation.

--- core/neo4j/latex_table.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-def csv_to_latex(filename):
-    df = pd.read_csv(filename)
-    df.to_latex
-
-csv_to_latex('node_count.csv')
--- neo4j/datasets-gdb.py
+++
@@ -1,7 +0,0 @@
-from py2neo import Graph, Node, Relationship
-
-
-
-
-
-graph = Graph("bolt://localhost:7687", user="neo4j", password="goku")
